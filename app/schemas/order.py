1b650b58592ad4d46e9eb3b90293761ce3e7296f
AHMAD Mohd Aameen Ext OBS/DIGITAL <JFVD7538@orange.com>
2025-12-13 11:10:00 +0530

    Added schema/order dependency for code to run

--- app/schemas/order.py
+++ app/schemas/order.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 from schemas.cart import CartItem
-from typing import List
+from typing import List, Literal
 
 
 class VerifyPaymentRequest(BaseModel):
@@ -10,9 +10,24 @@ class VerifyPaymentRequest(BaseModel):
     payment_id: str
     signature: str
 
+
+class OrderItemSchema(BaseModel):
+    product_id: int
+    title: str
+    image: Optional[str]
+    ordered_price: float
+    quantity: Optional[int] = 1
+    dimension: str
+    category: Optional[str]
+    subcategory: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+
 class OrderSchema(BaseModel):
     id: int
-    product_ids: List[int]
+    items: List[OrderItemSchema]
     razorpay_order_id: Optional[str]
     amount: float
     status: str
@@ -29,4 +44,6 @@ class OrderSchema(BaseModel):
 
 class CreateOrderRequest(BaseModel):
     items: List[CartItem]
+    address_id: int
+    payment_method: Literal["COD", "RAZORPAY"]
 
